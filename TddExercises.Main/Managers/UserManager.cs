ded6a1a43c752c95e52e7619e31e3d79b95cd49c
unknown <danilobjr@gmail.com>
2015-04-06 10:57:06 -0300

    created UserManager

---
+++ TddExercises.Main/Managers/UserManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddExercises.Main.Models;
+
+namespace TddExercises.Main.Managers
+{
+    public class UserManager
+    {
+        public bool Register(User newUser)
+        {
+            if (IsCredentialsValid(newUser))
+            {
+                InsertInDatabase(newUser);
+                SendEmailTo(newUser);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool IsCredentialsValid(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void InsertInDatabase(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SendEmailTo(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
