f6d11435c8731284ec1c1af6148574a3c77f9be6
unknown <danilobjr@gmail.com>
2015-04-05 23:19:31 -0300

    first commit

---
+++ TddExercises.Main.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddExercises.Main.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var isTrue = true;
+
+            Assert.IsTrue(isTrue);
+        }
+    }
+}
