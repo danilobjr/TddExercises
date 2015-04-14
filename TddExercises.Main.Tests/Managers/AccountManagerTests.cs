042da98c5b7c6a4319db2ae6507e0e71c52372f9
danilojrr <danilobjr@gmail.com>
2015-04-13 22:29:08 -0300

    test for RegisterNewUser() of AccountManager

---
+++ TddExercises.Main.Tests/Managers/AccountManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TddExercises.Main.Managers;
+using TddExercises.Main.Models;
+
+namespace TddExercises.Main.Tests.Managers
+{
+    [TestFixture]
+    public class AccountManagerTests
+    {
+        private User INVALID_USER = new User();
+
+        [Test]
+        public void RegisterNewUser_InvalidNewUser_ReturnsFalse()
+        {
+            // Arrange
+            var accountManager = new AccountManager();
+
+            // Act
+            var userWasRegistered = accountManager.RegisterNewUser(INVALID_USER);
+
+            // Assert
+            Assert.IsFalse(userWasRegistered);
+        }
+    }
+}
